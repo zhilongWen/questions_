9a4bce797ad099678732e34be7671fd0340659e5
zero <zero@zero.com>
2023-01-02 18:38:47 +0800

    jmh 线程池冷启动测试

---
+++ jmh/src/main/java/com/at/jmh/JMHSample_07_FixtureLevelInvocation.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright (c) 2014, Oracle America, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of Oracle nor the names of its contributors may be used
+ *    to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.at.jmh;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.*;
+
+/**
+ * Fixtures have different Levels to control when they are about to run.
+ * Level.Invocation is useful sometimes to do some per-invocation work
+ * which should not count as payload (e.g. sleep for some time to emulate
+ * think time)
+ */
+@Warmup(iterations = 1,time = 1,timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 1, time = 1, timeUnit = TimeUnit.SECONDS)
+@BenchmarkMode(value = Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+public class JMHSample_07_FixtureLevelInvocation {
+
+
+
+    @State(Scope.Benchmark) // 全局共用
+    public static class NormalState {
+        ExecutorService service;
+
+        @Setup(Level.Trial) // 一次测试调用一次
+        public void up() {
+            service = Executors.newCachedThreadPool();
+        }
+
+        @TearDown(Level.Trial) // 一次测试调用一次
+        public void down() {
+            service.shutdown();
+        }
+
+    }
+
+    /*
+     * This is the *extension* of the basic state, which also
+     * has the Level.Invocation fixture method, sleeping for some time.
+     */
+
+    public static class LaggingState extends NormalState {
+        public static final int SLEEP_TIME = Integer.getInteger("sleepTime", 10);
+
+        @Setup(Level.Invocation) // 方法每次调用都会被执行
+        public void lag() throws InterruptedException {
+            TimeUnit.MILLISECONDS.sleep(SLEEP_TIME);
+        }
+    }
+
+    /*
+     * This allows us to formulate the task: measure the task turnaround in
+     * "hot" mode when we are not sleeping between the submits, and "cold" mode,
+     * when we are sleeping.
+     */
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    public double measureHot(NormalState e, final Scratch s) throws ExecutionException, InterruptedException {
+        return e.service.submit(new Task(s)).get();
+    }
+
+    /**
+     * 模拟线程池冷启动
+     * @param e
+     * @param s
+     * @return
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    public double measureCold(LaggingState e, final Scratch s) throws ExecutionException, InterruptedException {
+        // LaggingState 每次执行都会睡  10 ms
+        return e.service.submit(new Task(s)).get();
+    }
+
+    /*
+     * This is our scratch state which will handle the work.
+     */
+
+    @State(Scope.Thread)
+    public static class Scratch {
+        private double p;
+        public double doWork() {
+            p = Math.log(p);
+            return p;
+        }
+    }
+
+    public static class Task implements Callable<Double> {
+        private Scratch s;
+
+        public Task(Scratch s) {
+            this.s = s;
+        }
+
+        @Override
+        public Double call() {
+            return s.doWork();
+        }
+    }
+
+    /*
+     * ============================== HOW TO RUN THIS TEST: ====================================
+     *
+     * You can see the cold scenario is running longer, because we pay for
+     * thread wakeups.
+     *
+     * You can run this test:
+     *
+     * a) Via the command line:
+     *    $ mvn clean install
+     *    $ java -jar target/benchmarks.jar JMHSample_07 -f 1
+     *    (we requested single fork; there are also other options, see -h)
+     *
+     * b) Via the Java API:
+     *    (see the JMH homepage for possible caveats when running from IDE:
+     *      http://openjdk.java.net/projects/code-tools/jmh/)
+     */
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(JMHSample_07_FixtureLevelInvocation.class.getSimpleName())
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+}
